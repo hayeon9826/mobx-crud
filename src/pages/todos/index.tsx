b15c958843ac2856a702a834cb1079b220dfc315
hayeon9826 <hayeonkim226@gmail.com>
2022-05-23 12:46:43 +0900

    edit todo list

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Observer } from 'mobx-react';
 import { Todo } from 'src/interface';
 import { toast } from 'react-toastify';
